4b5b50e3ee2933f7844ff85fda771087bd577a3f
Halil İbrahim Kalkan <hi_kalkan@yahoo.com>
2017-03-31 21:28:46 +0300

    Change pack.bat to pack.ps1

--- nupkg/pack.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-SET /P VERSION_SUFFIX=Please enter version-suffix (can be left empty): 
-
-dotnet "pack" "..\src\Castle.LoggingFacility.MsLogging" -c "Release" -o "." --version-suffix "%VERSION_SUFFIX%"
-
-pause
